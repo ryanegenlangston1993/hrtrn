05330ddea1dc211e6c17c7555b6cf71a7fb6019c
ryanegenlangston1993 <ryanegenlangston1993@gmail.com>
2021-04-10 20:32:54 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Can be multiple prefixes, like this: ("!", "?")
+BOT_PREFIX = ("!")
+TOKEN = "BOT TOKEN"
+APPLICATION_ID = "App ID"
+OWNERS = [Owner ID]
+BLACKLIST = []
+
+# Bot colors
+main_color = 0xD75BF4
+error = 0xE02B2B
+success = 0x42F56C
+warning = 0xF59E42
+info = 0x4299F5
